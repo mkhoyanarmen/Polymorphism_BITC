3a271d815a868a4c3f5030ae02191ceeb501a6b6
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-07-04 14:01:29 +0400

    Add project files.

---
+++ Polymorphism_BITC/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_BITC
+{
+    internal class Car
+    {
+        protected void StartEngine()
+        {
+            Console.WriteLine("The Engine is started");
+        }
+        public virtual void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("The Car is driven");
+        }
+    }
+}
---
+++ Polymorphism_BITC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_BITC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car sportCar = new SportCar();
+            sportCar.Drive();
+
+            Console.WriteLine();
+
+            Car car = new Car();
+            car.Drive();
+        }
+    }
+}
